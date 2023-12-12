29e7de89a4cd190477f6ebde3eb98f3dc94b6a06
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-12-12 19:09:40 +0600

    Add footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+    return (
+        <footer className="mb-10 px-4 text-center text-slate-500">
+            <small className="mb-2 text-xs block">
+                &copy; 2023 Nasif Ahmed. All rights reserved.
+            </small>
+
+            <p className="text-xs">
+                <span className="font-bold">About this website: </span> built
+                with React & Next.js (App Router & Server Actions), Typescript,
+                Tailwind CSS, Framer Motion, React Email and Resend, Vercel
+            </p>
+        </footer>
+    );
+}
