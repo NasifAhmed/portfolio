9cf7fec4b25d942db7aef271c269ac7291ca0868
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-12-13 01:29:50 +0600

    Fix email sending

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,8 +1,6 @@
 "use server";
 
-import { FaSortAmountDownAlt } from "react-icons/fa";
 import { Resend } from "resend";
-import { validateString } from "@/lib/utils";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -14,30 +12,11 @@ export const sendEmail = async (formData: FormData) => {
     const message = formData.get("message");
     const email = formData.get("email");
 
-    console.log(email);
-
-    // Simple server side validation
-    if (validateString(email, 500)) {
-        return {
-            error: "Invalid sender email",
-        };
-    }
-    if (validateString(message, 5000)) {
-        return {
-            error: "Invalidate message",
-        };
-    }
-    try {
-        await resend.emails.send({
-            from: "Contact Form <onboarding@resend.dev>",
-            to: "nasif2ahmed@gmail.com",
-            subject: "Message from contact from",
-            reply_to: email as string,
-            text: message as string,
-        });
-    } catch (error: any) {
-        return {
-            error: error.message,
-        };
-    }
+    resend.emails.send({
+        from: "Portfolio Contact Form <onboarding@resend.dev>",
+        to: "nasif2ahmed@gmail.com",
+        reply_to: email as string,
+        subject: "Message from contact from",
+        text: message as string,
+    });
 };
