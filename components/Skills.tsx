dff84676fdc2656f8c00ce6236082ac0c6a21ca3
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-12-12 19:09:30 +0600

    Add skills component

---
+++ components/Skills.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { skillsData } from "@/lib/data";
+import SectionHeading from "./SectionHeading";
+import { motion } from "framer-motion";
+
+const fadeInAnimationVariants = {
+    initial: {
+        opacity: 0,
+        y: 100,
+    },
+    animate: (index: number) => ({
+        opacity: 1,
+        y: 0,
+        transition: {
+            delay: 0.05 * index,
+        },
+    }),
+};
+
+export default function Skills() {
+    return (
+        <section
+            id="skills"
+            className="mb-28 sm:mb-40 max-w-3xl scroll-mt-28 text-center"
+        >
+            <SectionHeading text="My Skills" />
+            <ul className="flex flex-wrap justify-center items-center gap-2 text-lg text-slate-800">
+                {skillsData.map((skill, index) => {
+                    return (
+                        <motion.li
+                            className="bg-white border border-black/[0.1] rounded-sm px-5 py-3"
+                            key={index}
+                            variants={fadeInAnimationVariants}
+                            initial="initial"
+                            whileInView="animate"
+                            viewport={{
+                                once: true,
+                            }}
+                            custom={index}
+                        >
+                            {skill}
+                        </motion.li>
+                    );
+                })}
+            </ul>
+        </section>
+    );
+}
