937325308b1de5bfb76f61435d21824a007197f3
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-12-12 13:35:51 +0600

    Add backgroud circle color effects

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,13 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body
+                className={`${inter.className} bg-slate-200 text-slate-950 relative`}
+            >
+                <div className="bg-rose-200 absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] -z-10"></div>
+                <div className="bg-green-100 absolute top-[-1rem] left-[-35rem] h-[50rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] -z-10"></div>
+                {children}
+            </body>
         </html>
     );
 }
